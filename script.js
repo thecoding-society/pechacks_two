b464d66981e4b4105cee86bfeb09588500e6563d
praveenm1910 <praveenm191004@gmail.com>
2024-08-22 11:04:05 +0530

    FAQ's Added and Sponsers removed

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Bronze owl carousel */
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3000, // 3 seconds
+        autoplayHoverPause: false, // Do not pause on hover
+        smartSpeed: 1000, // Smooth transition
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3 // Display 3 items at a time
+            }
+        }
+    });
+  });
+  
