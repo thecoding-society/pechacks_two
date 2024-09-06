27a0297ea4579f6b5ef849df4f9018b436b2397b
praveenm1910 <praveenm191004@gmail.com>
2024-09-06 10:12:18 +0530

    FAQ's modified

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         loop: true,
-        margin: 10,
+        margin: 5,
         nav: true,
         autoplay: true,
         autoplayTimeout: 3000, // 3 seconds
